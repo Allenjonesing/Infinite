2b4e292a0d0f7cab11004b1b27b07277720667aa
Allenjonesing <Allenjonesing@gmail.com>
2024-06-07 09:08:47 -0500

    New API

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ async function fetchNews() {
     newsContainer.style.display = 'none';
 
     try {
-        const response = await fetch('https://infinite-4m22k9ivi-allen-jones-projects.vercel.app/news');
+        const response = await fetch('https://infinite-khkj21rcs-allen-jones-projects.vercel.app/news');
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
